6e50832ecedf3b158c9b4c20da4d3de99e9aee07
lai0xn <jnx.lain@gmail.com>
2024-07-09 19:58:06 +0100

    users service

--- go.mod
+++ go.mod
@@ -20,11 +20,16 @@ require (
 )
 
 require (
+	github.com/cweill/gotests v1.6.0 // indirect
+	golang.org/x/mod v0.18.0 // indirect
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/graphql-go/graphql v0.8.1 
-	github.com/graphql-go/handler v0.2.4 
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/graphql-go/graphql v0.8.1
+	github.com/graphql-go/handler v0.2.4
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 )
 
---
+++ internal/services/posts.go
@@ -0,0 +1,165 @@
+package services
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/lai0xn/squid-tech/pkg/types"
+	"github.com/lai0xn/squid-tech/pkg/utils"
+	"github.com/lai0xn/squid-tech/prisma"
+	"github.com/lai0xn/squid-tech/prisma/db"
+)
+
+type PostService struct{}
+
+func NewPostService() *PostService {
+	return &PostService{}
+}
+
+func (s *PostService) GetPost(id string) (*db.PostModel, error) {
+	ctx := context.Background()
+	utils.Logger.LogInfo().Fields(map[string]interface{}{
+		"query":  "get event",
+		"params": id,
+	}).Msg("DB Query")
+
+	result, err := prisma.Client.Post.FindUnique(
+		db.Post.ID.Equals(id),
+	).With(db.Post.Author.Fetch(),db.Post.Comments.Fetch()).Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+
+func (s *PostService) GetPosts(page int) ([]db.PostModel, error) {
+	ctx := context.Background()
+  limit := 10
+	result, err := prisma.Client.Post.FindMany().Skip((page-1)*limit).Take(limit).With(db.Post.Author.Fetch(),db.Post.Comments.Fetch()).Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+
+
+func (s *PostService) GetComment(id string) (*db.PostCommentModel, error) {
+	ctx := context.Background()
+	utils.Logger.LogInfo().Fields(map[string]interface{}{
+		"query":  "get event",
+		"params": id,
+	}).Msg("DB Query")
+
+	result, err := prisma.Client.PostComment.FindUnique(
+		db.PostComment.ID.Equals(id),
+	).With(db.PostComment.Author.Fetch()).Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+
+func (s *PostService) SearchPost(name string) ([]db.PostModel, error) {
+	ctx := context.Background()
+	utils.Logger.LogInfo().Fields(map[string]interface{}{
+		"query":  "search event",
+		"params": name,
+	}).Msg("DB Query")
+	result, err := prisma.Client.Post.FindMany(
+		db.Post.Or(
+        db.Post.Content.Contains(name),
+        db.Post.Description.Contains(name),
+    ),
+	).Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (s *PostService) UpdatePost(id string, payload types.EventPayload) (*db.PostModel, error) {
+	ctx := context.Background()
+	utils.Logger.LogInfo().Fields(map[string]interface{}{
+		"query":  "update org",
+		"id":     id,
+		"params": payload,
+	}).Msg("DB Query")
+	results, err := prisma.Client.Post.FindUnique(
+		db.Post.ID.Equals(id),
+	).Update(
+		db.Post.Content.Set(payload.Title),
+		db.Post.Description.Set(payload.Description),
+	).Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
+}
+
+func (s *PostService) CreatePost(userId string,content string, description string) (*db.PostModel, error) {
+
+	ctx := context.Background()
+	results, err := prisma.Client.Post.CreateOne(
+		db.Post.Content.Set(content),
+		db.Post.Description.Set(description),
+    db.Post.Author.Link(db.User.ID.Equals(userId)),
+
+	).Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
+}
+
+
+func (s *PostService) CommentPost(postid string, userid string,content string) (*db.PostCommentModel, error) {
+	utils.Logger.LogInfo().Fields(map[string]interface{}{
+		"query":   "create event",
+	}).Msg("DB Query")
+	ctx := context.Background()
+	results, err := prisma.Client.PostComment.CreateOne(
+		db.PostComment.Content.Set(content),
+    db.PostComment.Author.Link(db.User.ID.Equals(userid)),
+    db.PostComment.Post.Link(db.Post.ID.Equals(postid)),
+	).Exec(ctx)
+  if err != nil {
+    return nil,err
+  }
+  return results, nil
+}
+
+
+
+func (s *PostService) DeleteComment(id string) (*db.PostCommentModel, error) {
+	ctx := context.Background()
+	results, err := prisma.Client.PostComment.FindUnique(
+		db.PostComment.ID.Equals(id),
+	).Delete().Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
+}
+
+
+
+func (s *PostService) DeletePost(id string) (string, error) {
+	utils.Logger.LogInfo().Fields(map[string]interface{}{
+		"query":  "delete org",
+		"params": id,
+	}).Msg("DB Query")
+	ctx := context.Background()
+	deleted, err := prisma.Client.Post.FindUnique(
+		db.Post.ID.Equals(id),
+	).Delete().Exec(ctx)
+	if err != nil {
+		return "", nil
+	}
+	fmt.Println(deleted.ID)
+	return deleted.ID, nil
+}
+
+
